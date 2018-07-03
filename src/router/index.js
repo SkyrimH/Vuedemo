369b5e78ffdc0d9abbc654152a2a23c094488a2c
SkyrimH <huang.shan12@qq.com>
2018-07-04 00:00:04 +0800

    'router'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Homepage from '@/pages/home/Homepage'
+import List from '@/pages/list/List'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Homepage',
+      component: Homepage
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
