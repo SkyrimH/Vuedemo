682ec67348641ba751e64fc36e1a08d8b477758a
SkyrimH <huang.shan12@qq.com>
2018-07-12 20:45:07 +0800

    城市列表

---
+++ src/pages/city/components/list.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>            
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div> 
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                </div>
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                    <div class="item border-bottom">广州</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+    // https://github.com/ustbhuangyi/better-scroll
+    import Bscroll from 'better-scroll'
+    export default {
+        name: 'cityList',
+        mounted () {
+            this.scroll = new Bscroll(this.$refs.wrapper)
+        }
+    }
+</script>
+<style lang='stylus' scoped>
+    .border-topbottom
+        &:before
+            border-color: #cccccc
+        &:after
+            border-color: #cccccc
+    .border-bottom
+        &:before
+            border-color: #cccccc
+    .list
+        overflow: hidden
+        position: absolute
+        top: 1.6rem
+        left: 0
+        right: 0
+        bottom: 0
+        .title 
+            line-height: .44rem
+            background: #eee
+            padding-left: .2rem
+            color: #666
+        .button-list
+            overflow: hidden  //如果不添加此项，下面添加float属性的部分会脱离文档流，被后面的元素遮挡
+            padding: .1rem .6rem .1rem .1rem
+            .button-wrapper
+                float: left
+                width: 33.33%
+                .button
+                    margin: .1rem
+                    padding: .1rem 0
+                    text-align: center
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                padding:.2rem
+                line-height: .54rem
+</style>
